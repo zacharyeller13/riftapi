46660e8ad94034d34bc0f014950798071b4b3be0
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-18 10:04:57 -0500

    fix(json): needed JsonNull sentinel type (just copying dkjson.null) for propery "null" encoding

--- json.lua
+++ json.lua
@@ -1,31 +1,38 @@
 local dkjson = require("riftapi.lib.dkjson")
 local M = {}
 
+M.encode = dkjson.encode
+M.decode = dkjson.decode
+
+---@class JsonNull
+---@type JsonNull
+M.null = dkjson.null
+
 ---@param s string
 ---@return string
 function M._json_str(s)
-    return dkjson.encode(s) --[[@as string]]
+    return M.encode(s) --[[@as string]]
 end
 
----@param strings string[]|nil
+---@param strings? string[]
 ---@return string
 function M._json_arr(strings)
-    return dkjson.encode(strings or {}) --[[@as string]]
+    return M.encode(strings or {}) --[[@as string]]
 end
 
 ---@param v integer|nil
----@return integer|"null"
+---@return integer|JsonNull
 function M._json_opt_int(v)
-    return v or "null"
+    return v or M.null
 end
 
 ---@param v boolean|nil
----@return string
+---@return boolean|JsonNull
 function M._json_opt_bool(v)
-    return dkjson.encode(v) --[[@as string]]
+    if v == nil then
+        return M.null
+    end
+    return v
 end
-
-M.encode = dkjson.encode
-M.decode = dkjson.decode
 
 return M
--- spec/json_spec.lua
+++ spec/json_spec.lua
@@ -1,3 +1,4 @@
+---busted
 local json = require("riftapi.json")
 describe("Test json module", function()
     describe("Test _json_str", function()
@@ -10,19 +11,56 @@ describe("Test json module", function()
     end)
     describe("Test _json_opt_int", function()
         it("returns 'null' for nil", function()
-            assert.equal(json._json_opt_int(nil), "null")
+            assert.equal(json._json_opt_int(nil), json.null)
         end)
         it("returns int for int", function()
             assert.equal(json._json_opt_int(1), 1)
         end)
+        it("returns correct nested json with nil", function()
+            local a = { a = { b = nil } }
+            local b = { a = { b = json.null } }
+            local c = { a = { b = json._json_opt_int(nil) } }
+
+            assert.equal(json.encode(a), [[{"a":[]}]])
+            assert.equal(json.encode(b), [[{"a":{"b":null}}]])
+            assert.equal(json.encode(c), [[{"a":{"b":null}}]])
+
+            assert.are_not.equal(json.encode(a), json.encode(b))
+            assert.are_not.equal(json.encode(a), json.encode(c))
+            assert.equal(json.encode(b), json.encode(c))
+        end)
     end)
     describe("Test _json_opt_bool", function()
         it("returns 'null' for nil", function()
-            assert.equal(json._json_opt_bool(nil), "null")
+            assert.equal(json._json_opt_bool(nil), json.null)
         end)
         it("returns true/false for true/false", function()
-            assert.equal(json._json_opt_bool(true), "true")
-            assert.equal(json._json_opt_bool(false), "false")
+            assert.equal(json._json_opt_bool(true), true)
+            assert.equal(json._json_opt_bool(false), false)
+        end)
+        it("returns correct nested json with false", function()
+            local a = { a = { b = false } }
+            local b = { a = { b = json._json_opt_bool(false) } }
+
+            assert.equal(json.encode(a), json.encode(b), [[{"a":{"b":false}}]])
+        end)
+        it("returns correct nested json with true", function()
+            local a = { a = { b = true } }
+            local b = { a = { b = json._json_opt_bool(true) } }
+
+            assert.equal(json.encode(a), json.encode(b), [[{"a":{"b":true}}]])
+        end)
+        it("returns correct nested json with nil", function()
+            local a = { a = { b = nil } }
+            local b = { a = { b = json.null } }
+            local c = { a = { b = json._json_opt_bool(nil) } }
+
+            assert.equal(json.encode(a), [[{"a":[]}]])
+            assert.equal(json.encode(b), [[{"a":{"b":null}}]])
+            assert.equal(json.encode(c), [[{"a":{"b":null}}]])
+            assert.equal(json.encode(b), json.encode(c))
+            assert.are_not.equal(json.encode(a), json.encode(b))
+            assert.are_not.equal(json.encode(a), json.encode(c))
         end)
     end)
     describe("Test _json_arr", function()
@@ -35,7 +73,10 @@ describe("Test json module", function()
     end)
     describe("Test encode/decode", function()
         it("encodes nested tables", function()
-            assert.equal(json.encode({ a = "b", c = { d = { "e", "f" } } }), [[{"a":"b","c":{"d":["e","f"]}}]])
+            assert.equal(
+                json.encode({ a = "b", c = { d = { "e", "f" } } }, { keyorder = { "a", "c", "d" } }),
+                [[{"a":"b","c":{"d":["e","f"]}}]]
+            )
         end)
         it("decodes nested tables", function()
             assert.same(json.decode([[{"a":"b","c":{"d":["e","f"]}}]]), { a = "b", c = { d = { "e", "f" } } })
