3cb93d69b488dd749cebb99792dcafdd5e6b4f72
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-22 12:54:29 -0500

    refactor(mission_control): move mission_control methods to new execute

--- mission_control.lua
+++ mission_control.lua
@@ -3,17 +3,17 @@ M.mission_control = {}
 ---@return boolean|nil
 ---@return string|nil
 function M.mission_control.show_all()
-    return M.execute_legacy('{"Reactor":"show_mission_control_all"}')
+    return M.execute({ reactor = "show_mission_control_all" })
 end
 
 ---@return boolean|nil
 ---@return string|nil
 function M.mission_control.show_current()
-    return M.execute_legacy('{"Reactor":"show_mission_control_current"}')
+    return M.execute({ reactor = "show_mission_control_current" })
 end
 
 ---@return boolean|nil
 ---@return string|nil
 function M.mission_control.dismiss()
-    return M.execute_legacy('{"Reactor":"dismiss_mission_control"}')
+    return M.execute({ reactor = "dismiss_mission_control" })
 end
