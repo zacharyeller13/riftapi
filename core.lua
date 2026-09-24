5df073e2a3aec5eaa4a9c6880e70b01ad43fafc3
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-24 11:31:02 -0500

    refactor(core)!: remove execute_legacy method as all commands use the newer method

--- core.lua
+++ core.lua
@@ -68,25 +68,6 @@ function M.request(json_payload, await)
 end
 
 ---send an `execute_command` request
----@param command string must be a json-serialized RiftCommand string.
----@param args? string[]
----@return boolean|nil
----@return string|nil
----@deprecated Use typesafe payloads with execute
-function M.execute_legacy(command, args)
-    local p = string.format(
-        '{"execute_command":{"command":%s,"args":%s}}',
-        M.json._json_str(command),
-        M.json._json_arr(args or {})
-    )
-    local data, err = M.request(p)
-    if not data then
-        return nil, err
-    end
-    return true
-end
-
----send an `execute_command` request
 ---on success, return will be true, nil
 ---on failure, return will be nil, err
 ---@param command RiftCommand must be a json-serializable RiftCommand table
