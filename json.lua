67d043074533fb9b115a4bf6bbf5fa569a84082b
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2026-09-24 16:43:56 -0500

    refactor(json): remove now unused _json_str helper

--- json.lua
+++ json.lua
@@ -8,12 +8,6 @@ M.decode = dkjson.decode
 ---@type JsonNull
 M.null = dkjson.null
 
----@param s string
----@return string
-function M._json_str(s)
-    return M.encode(s) --[[@as string]]
-end
-
 ---@param strings? string[]
 ---@return string
 function M._json_arr(strings)
